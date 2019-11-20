a401d8b28ec90b1e1dc0b0d48256f83b4ffbfe74
vuhuyit99 <55819589+vuhuyit99@users.noreply.github.com>
2019-11-20 19:08:44 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,17 +6,5 @@ using System.Windows.Forms;
 
 namespace QLNhaTro
 {
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new TaoTK());
-        }
-    }
+    
 }
